08a7a1ec111a1b5ed0b7168cd9f25b2b2a78dfe7
David D Billingsley <daviddb31@hotmail.com>
2025-04-09 21:55:58 +0000

    think i found my issue in order.router.ts

--- server/src/features/customers/customers.router.ts
+++ server/src/features/customers/customers.router.ts
@@ -1,3 +1,33 @@
 import express from "express";
+import { getCustomerDetail, getCustomers, searchCustomers } from "./customers.service";
+import { getOrdersForCustomer } from "../orders/orders.service";
 
 export const customersRouter = express.Router();
+
+
+customersRouter.get("/", async (req, res) => {
+    const customers = await getCustomers();
+    res.json(customers);
+});
+
+customersRouter.get("/:id", async (req, res) => {
+    const id = req.params.id; 
+    const customer = await getCustomerDetail(id);
+    if (customer != null ) {
+        res.json(customer);
+    } else {
+        res.status(404).json({message: "Customer Not Found" });
+    }
+    });
+
+    customersRouter.get("/:id/orders", async (req, res) => {
+        const id = req.params.id;
+        const orders = await getOrdersForCustomer(id);
+        res.json(orders);
+    });
+
+    customersRouter.get("/search/:query", async (req, res) => {
+        const query = req.params.query;
+        const customers = await searchCustomers(query);
+        res.json(customers);
+    });
--- server/src/features/orders/orders.router.ts
+++ server/src/features/orders/orders.router.ts
@@ -1,3 +1,38 @@
 import express from "express";
+import { getOrderDetail, getOrders } from "./orders.service";
 
 export const ordersRouter = express.Router();
+
+ordersRouter.get("/", async (req, res) => {
+    const query = req.query;
+    const take = query.take;
+    const skip = query.skip;
+
+    if (
+        take &&
+        typeof take === "string" &&
+        parseInt(take) > 0 &&
+        skip &&
+        typeof skip === "string" &&
+        parseInt(skip) > -1 
+    ) {
+    const orders = await getOrders(parseInt(skip), parseInt(take));
+    res.json(orders);
+    } else {
+        res.status(400).json({
+            message:
+            "Take and skip query parameters are required. " +
+            "Take must be greater than 0 and skip must be greater than -1",
+        });
+    }
+});
+
+ordersRouter.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    const order = await getOrderDetail(id);
+    if (order != null) {
+        res.json(order);
+    } else {
+        res.status(404).json({message: "Order Not Found"});
+    }
+});
