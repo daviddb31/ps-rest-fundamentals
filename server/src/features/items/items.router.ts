7e2ff2ab8a6a7d3a75f01cd671f9758180f7ee54
David D Billingsley <daviddb31@hotmail.com>
2025-04-09 21:28:12 +0000

    no idea how to fix it

--- server/src/features/items/items.router.ts
+++ server/src/features/items/items.router.ts
@@ -2,6 +2,14 @@ import express from "express";
 
 export const itemsRouter = express.Router();
 
+itemsRouter.get("/", async (req, res)=>{
+  const items = await getItems();
+  items.forEach((item) => {
+    items.imageUrl = buildImageUrl(req, items.id);
+  });
+  res.json(items)
+});
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 function buildImageUrl(req: any, id: number): string {
   return `${req.protocol}://${req.get("host")}/images/${id}.jpg`;
